beae05eb198631af6ebbe4f4b1c4fb4e007b037f
Tim Lestander <timlestander@gmail.com>
2023-09-07 16:08:09 +0200

    fix: api request to https

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,11 +8,11 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   public getLeagueData(): Observable<any> {
-    return this.http.get('http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard');
+    return this.http.get('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard');
   }
 
   public getTeamData(id: number): Observable<any> {
-    return this.http.get(`http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${id}`);
+    return this.http.get(`https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${id}`);
   }
 
 }
